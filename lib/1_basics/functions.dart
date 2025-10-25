eb910208b4a88b8711dfa44a2666cdf96ef34d70
Krishna Sharma <sharmakriss985@gmail.com>
2025-10-25 16:39:31 +0530

    Functions Practice Updated

--- lib/1_basics/functions.dart
+++ lib/1_basics/functions.dart
@@ -5,7 +5,7 @@ void sayHello() {
   debugPrint("Hello, world");
 }
 
-// Addition function
+// Add function
 int add(int a, int b) {
   return a + b;
 }
@@ -34,11 +34,32 @@ void defaultParameter({String name = "Kris", int age = 19}) {
   debugPrint("The default name is $name, and default age is $age");
 }
 
+//=============================
+// Arrow Syntax Function Defining
+//=============================
+
+// sayHelo Function
+void sayHelo() => debugPrint("This is also called fat arrow function");
+
+// Adding Function
+int addition(int a, int b) => a + b;
+
+// Arrow function with void
+void arrow(String name) => debugPrint("Hello, $name");
+
+//=============================
+// Higher Order Function Defining
+//=============================
+
+void callFunction(void Function() func) {
+  func;
+}
+
 void main() {
   // Calling sayHello function
   sayHello();
 
-  // Calling addition function
+  // Calling add function
   int result = add(5, 3);
   debugPrint("Result: $result");
 
@@ -59,4 +80,47 @@ void main() {
   //=============================
 
   defaultParameter(name: "Kris");
+
+  //=============================
+  // Anonymous Function
+  //=============================
+
+  // Assigning anonymous function to a variable
+  var anonymous = () {
+    debugPrint("Hello from anonymous function");
+  };
+
+  // Calling that function
+  anonymous();
+
+  // Using Anonymous function directly to in forEach
+
+  List<String> fruits = ["Mango", "Banana", "Apple"];
+  for (var fruit in fruits) {
+    debugPrint("Fruits:$fruit");
+  }
+
+  //=============================
+  // Arrow Syntax Function Calling
+  //=============================
+
+  // sayHelo Calling
+  sayHelo();
+
+  // Addition Function Returning Calling
+  int sum = addition(5, 3);
+  debugPrint("Sum Using Arrow Function: $sum");
+
+  // Arrow function with void calling
+  arrow("Kris");
+
+  //Arrow function as callback by forEach
+  List<int> numbers = [1, 2, 3];
+  numbers.forEach((n) => debugPrint("Numbers: $n"));
+
+  //=============================
+  // Higher Order Function Calling
+  //=============================
+
+  callFunction(sayHelo);
 }
